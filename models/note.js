6cfa34dc7ab3b8192a4f9d018eb5218e12829ebf
fongmile <70105744+fongmile@users.noreply.github.com>
2023-08-17 21:29:18 +0100

    log url

--- models/note.js
+++ models/note.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery',false)
 
 const url = process.env.MONGODB_URI
-console.log('connecting to MongoDB')
+console.log('connecting to MongoDB', url)
 
 mongoose.connect(url)
 	.then(result => {
